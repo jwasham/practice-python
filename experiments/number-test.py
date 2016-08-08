3b5f63e66378ec02e93e2ce70217e6ab354fe936
John Washam <john@readyprompt.com>
2016-08-08 14:07:07 -0700

    Added experiment for file handling.

---
+++ experiments/number-test.py
@@ -0,0 +1,19 @@
+import os
+
+
+def main():
+    input_file = "numbers.txt"
+    output_file = "numbers-mult.txt"
+
+    output = []
+
+    with open(input_file, "r") as input_handle:
+        for line_number, line in enumerate(input_handle, 1):
+            output.append(line_number * int(line.rstrip()))
+
+    output_handle = open(output_file, "w")
+    output_handle.write(os.linesep.join(str(n) for n in output))
+
+
+if __name__ == "__main__":
+    main()
