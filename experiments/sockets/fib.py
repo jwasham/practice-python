d399ad7dfa528aa540491dc5108c7b06dd437641
John Washam <john@readyprompt.com>
2016-08-20 11:21:51 -0700

    Sockets and threads.

---
+++ experiments/sockets/fib.py
@@ -0,0 +1,10 @@
+"""
+Really bad Fibonacci implementation
+"""
+
+
+def fib(n):
+    if n <= 2:
+        return 1
+
+    return fib(n - 1) + fib(n - 2)
---
+++ experiments/sockets/server.py
@@ -0,0 +1,34 @@
+"""
+Microservice for Fibonacci
+"""
+
+from socket import *
+from fib import fib
+
+
+def fib_server(address):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    sock.bind(address)
+    sock.listen(5)
+
+    while True:
+        client, addr = sock.accept()
+        print("Connected on {}", addr)
+        fib_handler(client)
+
+
+def fib_handler(client):
+    while True:
+        req = client.recv(100)
+        if not req:
+            break
+        n = int(req)
+        result = fib(n)
+        resp = str(result).encode('ascii') + b'\n'
+        client.send(resp)
+
+    print("Closed")
+
+
+fib_server(('', 25000))
