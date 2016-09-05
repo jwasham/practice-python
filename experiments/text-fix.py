fdce8ecd3dd432893f67348e165e0f55a4910861
John Washam <john@readyprompt.com>
2016-09-05 12:14:20 -0700

    Removed unneeded items.

--- experiments/text-fix.py
+++ experiments/text-fix.py
@@ -4,21 +4,6 @@ import ftfy
 def main():
     print(ftfy.fix_encoding('ì£¼ë¬¸í•˜ë‹¤ - to intent for?'))
 
-    runs = 0
-
-    start = 1
-    stop = 6
-    step = 1
-
-    # not totally accurate - at least one iteration when stop > start
-    iterations = (stop - start) // step
-
-    for i in range(start, stop, step):
-        runs += 1
-
-    print(runs)
-    print(iterations)
-
 
 if __name__ == '__main__':
     main()
