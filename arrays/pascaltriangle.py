80e4495ea63c09007d65ee0b0c6d5c728fb1820b
John Cancio <jmikecancio@gmail.com>
2022-04-28 11:33:42 +0100

    Pascal Triangle exercise

--- arrays/pascaltriangle.py
+++ arrays/pascaltriangle.py
@@ -1,20 +1,22 @@
-#Implement pascal's triangle using lists
+# Implement pascal's triangle using lists
 
 def triangle_list(k):
-    tr = []
-    row = []
+    result_v = []
+    row_v = []
     for i in range(k):
-        row.insert(0,1)
-        for j in range(1,len(row)-1):
-            if j < len(row)-1:
-                row[j] = row[j] + row[j+1]
-        tr.append(list(row))
-    return tr
+        row_v.insert(0,1)
+        for j in range(1,len(row_v)-1):
+            if j < len(row_v)-1:
+                row_v[j] = row_v[j] + row_v[j+1]
+        result_v.append(list(row_v))
+    return result_v
+
 
 def main():
-    input = 7
-    print("Input: ", input)
-    print("Output: ", format(triangle_list(input)))
+    input_v = 5
+    print("Input: ", input_v)
+    print("Output: ", format(triangle_list(input_v)))
+
 
 if __name__ == "__main__":
     main()
